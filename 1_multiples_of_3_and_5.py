91b2b5f08c0ad9dd00862ffe4271f486de461349
Brenden Smith <bcsmith2846@gmail.com>
2021-04-03 18:23:38 -0500

    Went back and added comments to previous code

--- 1_multiples_of_3_and_5.py
+++ 1_multiples_of_3_and_5.py
@@ -1,8 +1,13 @@
+# We're trying to find the sum of numbers divisible by 3 or 5
+# starting at 3 and going up to `max_num`
 def solution(max_num: int) -> int:
+    # Loop from 3 (smallest possible answer) to `max_num`
+    # and return the `answer`
     answer: int = 0
-    for x in range(max_num):
+    for x in range(3, max_num):
         answer += x if (x % 3 == 0 or x % 5 == 0) else 0
     return answer
 
 
+# Boilerplate runner code
 print(solution(1000))
