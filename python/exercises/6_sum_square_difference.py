bd72be8367982b9d01577e3ff1bdad245f05ea89
Brenden Smith <bcsmith2846@gmail.com>
2021-04-04 01:35:22 -0500

    Add skeleton to exercise 6

--- python/exercises/6_sum_square_difference.py
+++ python/exercises/6_sum_square_difference.py
@@ -3,14 +3,21 @@ import math
 import itertools
 from functools import reduce
 
-# Description of the problem
+# Find the difference betwenn the square of the sums of the numbers
+# `1..end` and the sum of the squares of the numbers `1..end`
+#
+# Sum of the squares 1..10:
+#       1^2 + 2^2 + ... + 10^2 = 385
+# Square of the sums 1..10:
+#       (1 + 2 + ... + 10)^2 = 3025
 
 
 class Solution:
 
-    def solution(param: type) -> returnType:
+    def solution(self, end: int) -> int:
         pass
 
 
-input = []
-print(Solution().solution(input))
+input1 = 10
+input2 = 100
+print(Solution().solution(input1))
