845f38daacf267a80e00596cfd9f02b3c85a3db1
Brenden Smith <bcsmith2846@gmail.com>
2021-04-04 01:20:03 -0500

    Add skeletons of files for the next milestone

---
+++ python/10_summation_of_primes.py
@@ -0,0 +1,16 @@
+from typing import List, Tuple
+import math
+import itertools
+from functools import reduce
+
+# Description of the problem
+
+
+class Solution:
+
+    def solution(param: type) -> returnType:
+        pass
+
+
+input = []
+print(Solution().solution(input))
